33dc9e6affbff7f00f00828bae3a56909274435c
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2023-02-25 21:58:18 +0200

    Completely rewrite the node in Neutralino API

--- src/index.js
+++ src/index.js
@@ -1,50 +1,47 @@
-const { spawn } = require('child_process');
-
+const { execCommand } = require("@neutralinojs/neu");
 const form = document.querySelector('#download-form');
 const output = document.querySelector('#output');
 
 form.addEventListener('submit', (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-const appId = document.querySelector('#app-id').value;
-const depotId = document.querySelector('#depot-id').value;
-const manifestId = document.querySelector('#manifest-id').value;
-const username = document.querySelector('#username').value;
-const password = document.querySelector('#password').value;
+  const appId = document.querySelector('#app-id').value;
+  const depotId = document.querySelector('#depot-id').value;
+  const manifestId = document.querySelector('#manifest-id').value;
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
 
-const platforms = Array.from(document.querySelectorAll('input[name="platform"]:checked')).map(input => input.value);
+  const platforms = Array.from(document.querySelectorAll('input[name="platform"]:checked')).map(input => input.value);
 
-if (platforms.length === 0) {
-output.innerHTML = 'Please select at least one platform.';
-return;
-}
+  if (platforms.length === 0) {
+    output.innerHTML = 'Please select at least one platform.';
+    return;
+  }
 
-const args = [
---app-id=${appId},
---depot-id=${depotId},
---manifest-id=${manifestId},
---username=${username},
---password=${password},
-...platforms.map(platform => --${platform}),
-];
+  const args = [
+    `--app-id=${appId}`,
+    `--depot-id=${depotId}`,
+    `--manifest-id=${manifestId}`,
+    `--username=${username}`,
+    `--password=${password}`,
+    ...platforms.map(platform => `--${platform}`),
+  ];
 
-const process = spawn('DepotDownloader.exe', args);
+  const command = `./DepotDownloader ${args.join(' ')}`;
+  output.innerHTML = 'Downloading...';
 
-output.innerHTML = 'Downloading...';
-
-process.stdout.on('data', (data) => {
-console.log(data.toString());
-});
-
-process.stderr.on('data', (data) => {
-console.error(data.toString());
-});
-
-process.on('exit', (code) => {
-if (code === 0) {
-output.innerHTML = 'Download completed successfully.';
-} else {
-output.innerHTML = Download failed with code ${code}.;
-}
-});
+  execCommand(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      output.innerHTML = `Error: ${error.message}`;
+      return;
+    }
+    if (stderr) {
+      console.error(stderr);
+      output.innerHTML = `Error: ${stderr}`;
+      return;
+    }
+    console.log(stdout);
+    output.innerHTML = 'Download completed successfully.';
+  });
 });
